a497e098912ba34a023c35808916ec7966e49a47
Slavik0041 <73832000+Slavik0041@users.noreply.github.com>
2021-04-05 15:13:15 +0300

    bh_7_tasks Ready easy -> abstract_class

--- bh_7_tasks-master/easy/abstract_class/animals.py
+++ bh_7_tasks-master/easy/abstract_class/animals.py
@@ -9,3 +9,53 @@
 На основе Animal определить классы Cat, Dog, Cow, которые переопределят метод says,
 чтобы он выводил, например "Кошка {name} говорит МЯУ"
 """
+from abc import ABC
+
+
+class Animal(ABC):
+    name: str
+    a_type: str
+
+    def __init__(self, name, a_type):
+        self.name = name
+        self.a_type = a_type
+
+    def says(self):
+        print(f'Животное по кличке {self.name} ')
+
+
+class Cat(Animal):
+
+    def __init__(self, name, a_type):
+        super().__init__(name, a_type)
+
+    def says(self):
+        super().says()
+        print(f'{self.a_type} кошка {self.name} говорит МЯУ \n')
+
+
+class Dog(Animal):
+
+    def __init__(self, name, a_type):
+        super().__init__(name, a_type)
+
+    def says(self):
+        super().says()
+        print(f'{self.a_type} пес {self.name} говорит ГАФ \n')
+
+
+class Cow(Animal):
+    def __init__(self, name, a_type):
+        super().__init__(name, a_type)
+
+    def says(self):
+        super().says()
+        print(f'{self.a_type} корова {self.name} говорит МУУ ')
+
+
+cat = Cat('Анфиса', 'Домашняя')
+dog = Dog('Барбос', 'Домашний')
+cow = Cow('Мурка', 'Домашняя')
+Cat.says(cat)
+Dog.says(dog)
+Cow.says(cow)
--- bh_7_tasks-master/easy/abstract_class/geometry.py
+++ bh_7_tasks-master/easy/abstract_class/geometry.py
@@ -22,3 +22,72 @@
 Площадь = a ** 2
 """
 from math import pi
+from abc import ABC
+
+
+class Share(ABC):
+    def get_perimeter(self):
+        print(f'Посчитаем периметр фигуры: ')
+
+    def get_square(self):
+        print(f'Посчитаем площадь фигуры: ')
+
+
+class Circle(Share):
+    r: int
+
+    def __init__(self, r=0):
+        self.r = r
+
+    def get_perimeter(self, r):
+        dlina = 2 * pi * r
+        print(f'Длина окружности с радиусом {r}: {dlina} ')
+
+    def get_square(self, r):
+        square = pi * r ** 2
+        print(f'Площадь окружности с радиусом {r}: {square} \n')
+
+
+class Rectangle(Share):
+    a: int
+    b: int
+
+    def __init__(self, a=0, b=0):
+        self.a = a
+        self.b = b
+
+    def get_perimeter(self, a, b):
+        perimetr = (a + b) * 2
+        print(f'Периметр прямоугольника со сторонами '
+              f'а = {a}, b = {b}: {perimetr} ')
+
+    def get_square(self, a, b):
+        perimetr = a * b
+        print(f'Площадь прямоугольника со сторонами '
+              f'а = {a}, b = {b}: {perimetr} \n')
+
+
+class Square(Rectangle):
+    def __init__(self, a=0):
+        super().__init__(a)
+
+    def get_perimeter(self, a):
+        perimetr = 4 * a
+        print(f'Периметр квадрата со стороной а = {a}: {perimetr} ')
+
+    def get_square(self, a):
+        perimetr = a ** 2
+        print(f'Площадь квадрата со стороной а = {a}: {perimetr} \n')
+
+
+circle = Circle()
+Circle.get_perimeter(circle, 6)
+Circle.get_square(circle, 6)
+
+rectangle = Rectangle()
+Rectangle.get_perimeter(rectangle, 3, 4)
+Rectangle.get_square(rectangle, 3, 4)
+
+square = Square()
+Square.get_perimeter(square, 5)
+Square.get_square(square, 5)
